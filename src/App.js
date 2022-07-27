947be6ae1bb9590a1647c13d0cd9f8856a300c0a
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-07-28 00:24:50 +0600

    added all files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://i.ibb.co/7vgbS1q/kiron3-modified.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Job Matchers - Something big is coming....
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/kiron0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit GitHub
         </a>
       </header>
     </div>
